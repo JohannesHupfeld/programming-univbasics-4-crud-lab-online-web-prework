b02f599a87a0f7e6d2929fd80b6302d1fb7326d5
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-09-08 02:01:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   famous_cats = ["tom","grumpy cat","maru"]
   famous_cats << "nala cat"
+  p famous_cats
 end
 
 def add_element_to_start_of_array(array, element)
